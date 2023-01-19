d2e7319633f357ac9d672496f392b64d2029b6d3
vadim homutovsky <vadimhomutovsky@gmail.com>
2023-01-19 15:23:22 +0300

    fix wrongs with wsitcher

--- index.js
+++ index.js
@@ -11,37 +11,35 @@ img.addEventListener('mouseout', () => {
   buttonLeft.style = "display:block"
   buttonRight.style = "display:block"
 })
+
+let count = 0
+const photoArray = ["img/person-photo.jpg", "img/person-photo2.jpg", "img/person-photo3.jpg", "img/person-photo4.jpg", "img/person-photo5.jpg", "img/person-photo6.jpg"]
+
 buttonRight.addEventListener('click', () => {
-if(img.src == 'http://127.0.0.1:5501/img/person-photo.jpg') {
-  buttonLeft.style = 'backgroundImage: url(img/arrow-right.svg); border:solid 1px #fff; cursor:pointer'
-  img.src = 'http://127.0.0.1:5501/img/person-photo2.jpg'
-} else if(img.src == 'http://127.0.0.1:5501/img/person-photo2.jpg') {
-  img.src = 'http://127.0.0.1:5501/img/person-photo3.jpg'
-} else if(img.src == 'http://127.0.0.1:5501/img/person-photo3.jpg') {
-  img.src = 'http://127.0.0.1:5501/img/person-photo4.jpg'
-} else if(img.src == 'http://127.0.0.1:5501/img/person-photo4.jpg') {
-  img.src = 'http://127.0.0.1:5501/img/person-photo5.jpg'
-} else if(img.src == 'http://127.0.0.1:5501/img/person-photo5.jpg') {
-  img.src = 'http://127.0.0.1:5501/img/person-photo6.jpg'
-  buttonRight.style = "background:none; border:none; cursor:none"
-} 
+  debugger
+  if (count < 5) {
+    count++
+    if (count === 1) {
+      buttonLeft.style = 'backgroundImage: url(img/arrow-left.svg); border:solid 1px #fff; cursor:pointer'
+
+    } else if (count === 5) {
+      buttonRight.style = "background:none; border:none; cursor:none"
+    }
+
+    img.src = photoArray[count]
+
+  }
+
 })
 
 
 buttonLeft.addEventListener('click', () => {
-  if(img.src == 'http://127.0.0.1:5501/img/person-photo6.jpg') {
-    img.src = 'http://127.0.0.1:5501/img/person-photo5.jpg'
-    buttonRight.style = 'backgroundImage: url(img/arrow-left.svg); border:solid 1px #fff; cursor:pointer'
-  } else if(img.src == 'http://127.0.0.1:5501/img/person-photo5.jpg') {
-    img.src = 'http://127.0.0.1:5501/img/person-photo4.jpg'
-  } else if(img.src == 'http://127.0.0.1:5501/img/person-photo4.jpg') {
-    img.src = 'http://127.0.0.1:5501/img/person-photo3.jpg'
-  } else if(img.src == 'http://127.0.0.1:5501/img/person-photo3.jpg') {
-    img.src = 'http://127.0.0.1:5501/img/person-photo2.jpg'
-  } else if(img.src == 'http://127.0.0.1:5501/img/person-photo2.jpg') {
-    img.src = 'http://127.0.0.1:5501/img/person-photo.jpg'
-    buttonLeft.style = "background:none; border:none; cursor:none"
-  } 
-  })
-
-
+  if (count > 0) {
+    if (count === 1) {
+      buttonLeft.style = "background:none; border:none; cursor:none"
+    }
+    count--
+    buttonRight.style = 'backgroundImage: url(img/arrow-right.svg); border:solid 1px #fff; cursor:pointer'
+    img.src = photoArray[count]
+  }
+})
